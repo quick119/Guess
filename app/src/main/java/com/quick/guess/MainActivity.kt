30d8058bbc47dc38caaf7ee5dde87fab2a0d6f2b
Simon Wang <quick.wang@gmail.com>
2019-04-24 13:34:20 +0800

    單元5

--- app/src/main/java/com/quick/guess/MainActivity.kt
+++ app/src/main/java/com/quick/guess/MainActivity.kt
@@ -2,12 +2,21 @@ package com.quick.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+    fun check(view : View) {
+        val n = ed_number.text.toString().toInt()
+        println("number: $n")
+        Log.d("MainActivity", "number:" + n)
     }
 }
