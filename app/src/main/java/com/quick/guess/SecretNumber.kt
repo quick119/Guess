fd1c16f04ea671970cb707e54790d0e70ebdc4b9
Simon Wang <quick.wang@gmail.com>
2019-05-08 15:35:37 +0800

    單元8

--- app/src/main/java/com/quick/guess/SecretNumber.kt
+++ app/src/main/java/com/quick/guess/SecretNumber.kt
@@ -3,11 +3,16 @@ package com.quick.guess
 import java.util.*
 
 class SecretNumber {
-    val secret: Int = Random().nextInt(10) + 1
+    var secret: Int = Random().nextInt(10) + 1
     var count = 0
 
     fun validate(number : Int) : Int {
         count ++
         return number - secret
+    }
+
+    fun reset() {
+        secret = Random().nextInt(10) + 1
+        count = 0
     }
 }
