5e27f2ace171421ab619708cd17be4bff264f4ac
Simon Wang <quick.wang@gmail.com>
2019-05-08 15:08:53 +0800

    單元7

--- app/src/main/java/com/quick/guess/MainActivity.kt
+++ app/src/main/java/com/quick/guess/MainActivity.kt
@@ -10,28 +10,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val secretNumber = SecretNumber()
-
+    val TAG = MainActivity::class.java.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "serect:"+secretNumber.secret)
+        Log.d(TAG, "serect:" + secretNumber.secret)
     }
-    fun check(view : View) {
-        val n = ed_number.text.toString().toInt()
-        println("number: $n")
-        Log.d("MainActivity", "number:" + n)
-        val diff = secretNumber.validate(n)
-        var message = "Yes, you got it"
-        if (diff < 0) {
-            message = "Bigger"
-        } else if (diff > 0) {
-            message = "Smaller"
-        }
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-        AlertDialog.Builder(this)
-            .setTitle("Message")
-            .setMessage(message)
-            .setPositiveButton("OK", null)
-            .show()
-    }
+   fun check(view : View) {
+       val n = ed_number.text.toString().toInt()
+       println("number: $n")
+       Log.d(TAG, "n:" + n)
+       val diff = secretNumber.validate(n)
+       var message = getString(R.string.yes_you_got_it)
+       if (diff < 0) {
+           message = getString(R.string.bigger)
+       } else if (diff > 0) {
+           message = getString(R.string.smaller)
+       }
+//       Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+       AlertDialog.Builder(this)
+           .setTitle(getString(R.string.dialog_title))
+           .setMessage(message)
+           .setPositiveButton(getString(R.string.ok), null)
+           .show()
+   }
 }
