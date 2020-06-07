eded5417813e3c2e0856e14b4c2adb6f5c00dafa
quick <quick.wang@gmail.com>
2020-06-07 19:20:08 +0800

    21. Room 實作，設計 Entity、DAO、Database

--- app/src/main/java/com/quick/guess/data/GameDatabase.kt
+++ app/src/main/java/com/quick/guess/data/GameDatabase.kt
@@ -1,2 +1,10 @@
 package com.quick.guess.data
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Record::class, Word::class), version = 1)
+abstract class GameDatabase : RoomDatabase() {
+    abstract fun recordDao() : RecordDao
+}
+
