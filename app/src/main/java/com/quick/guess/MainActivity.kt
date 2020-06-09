8ea936dd6afbe2744d90ae81fe9b513f2049255d
quick <quick.wang@gmail.com>
2020-06-09 15:17:28 +0800

    24. RecyclerView 清單元件的原理與實作，使用集合 (List)

--- app/src/main/java/com/quick/guess/MainActivity.kt
+++ app/src/main/java/com/quick/guess/MainActivity.kt
@@ -3,35 +3,55 @@ package com.quick.guess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_function.view.*
 
 class MainActivity : AppCompatActivity() {
-    val secretNumber = SecretNumber()
     val TAG = MainActivity::class.java.simpleName
+    val functions = listOf<String>(
+        "Camera",
+        "Invite friend",
+        "Parking",
+        "Download coupons",
+        "News",
+        "Maps")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "serect:" + secretNumber.secret)
+        //RecyclerView
+
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.setHasFixedSize(true)
+        recycler.adapter = FunctionAdapter()
     }
-   fun check(view : View) {
-       val n = ed_number.text.toString().toInt()
-       println("number: $n")
-       Log.d(TAG, "n:" + n)
-       val diff = secretNumber.validate(n)
-       var message = getString(R.string.yes_you_got_it)
-       if (diff < 0) {
-           message = getString(R.string.bigger)
-       } else if (diff > 0) {
-           message = getString(R.string.smaller)
-       }
-//       Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-       AlertDialog.Builder(this)
-           .setTitle(getString(R.string.dialog_title))
-           .setMessage(message)
-           .setPositiveButton(getString(R.string.ok), null)
-           .show()
-   }
+
+    inner class FunctionAdapter() : RecyclerView.Adapter<FunctionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FunctionHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_function, parent, false)
+            val holder = FunctionHolder(view)
+            return holder
+        }
+
+        override fun getItemCount(): Int {
+            return functions.size
+        }
+
+        override fun onBindViewHolder(holder: FunctionHolder, position: Int) {
+            holder.nameText.text = functions.get(position)
+        }
+
+    }
+
+    class FunctionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var nameText: TextView = view.name
+    }
 }
