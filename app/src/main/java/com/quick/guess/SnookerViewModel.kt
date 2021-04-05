51979841ceeb7d447bc8c0d6e49f269eae18823e
Simon Wang <quick119@hotmail.com>
2021-04-05 18:36:47 +0800

    49. 在 MVVM 架構中使用 Coroutines 進行網路連線

--- app/src/main/java/com/quick/guess/SnookerViewModel.kt
+++ app/src/main/java/com/quick/guess/SnookerViewModel.kt
@@ -6,15 +6,16 @@ import androidx.lifecycle.viewModelScope
 import com.google.gson.Gson
 import com.quick.guess.data.Event
 import com.quick.guess.data.EventResult
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.net.URL
 
 class SnookerViewModel: ViewModel() {
     val events = MutableLiveData<List<Event>>()
     init {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             val data = URL("http://api.snooker.org/?t=5&s=2020").readText()
-            events.value = Gson().fromJson(data, EventResult::class.java)
+            events.postValue(Gson().fromJson(data, EventResult::class.java))
         }
     }
 }
